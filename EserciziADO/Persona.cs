621b4add97b5bbe47242f30428ae00af97e6e33b
Roberto Spagliccia <Spacio@users.noreply.github.com>
2019-10-21 22:18:02 +0200

    changed Persona_ID from int to Guid and added to ctors

--- EserciziADO/Persona.cs
+++ EserciziADO/Persona.cs
@@ -8,20 +8,21 @@ namespace EserciziADO
 {
     public class Persona
     {
-        public int Persona_ID { get; set; }
+        public Guid Persona_ID { get; set; }
         public string Nome { get; set; }
         public string Cognome { get; set; }
         public DateTime DataDiNascita { get; set; }
 
         public Persona()
         {
-
+            Persona_ID = Guid.NewGuid();
         }
         public Persona(string nome, string cognome, DateTime dataDiNascita)
         {
             Nome = nome;
             Cognome = cognome;
             DataDiNascita = dataDiNascita;
+            Persona_ID = Guid.NewGuid();
         }
 
         public override string ToString()
