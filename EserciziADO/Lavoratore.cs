1dd362864d0184ddf14afa4df51ffac2c195d0e8
Roberto Spagliccia <Spacio@users.noreply.github.com>
2019-10-21 20:54:37 +0200

    added Lavoratori class

---
+++ EserciziADO/Lavoratore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EserciziADO
+{
+    public enum TipoLavoratore
+    {
+        Autonomo,
+        Dipendente
+    }
+
+    public class Lavoratore : Persona
+    {
+        #region Properties
+        public double Retribuzione { get; set; }
+        public double RAL
+        {
+            get
+            {
+                return Retribuzione * GetMensilità();
+            }
+        }
+        public DateTime? DataDiAssunzione { get; set; }
+        public TipoLavoratore Tipo { get; set; }
+        #endregion
+
+        #region Costruttori
+        public Lavoratore()
+        {
+
+        }
+        public Lavoratore(string nome, string cognome, DateTime dataDiNascita, double retribuzione, DateTime? dataAssunzione, TipoLavoratore tipo)
+            : base(nome, cognome, dataDiNascita)
+        {
+            Retribuzione = retribuzione;
+            DataDiAssunzione = dataAssunzione;
+            Tipo = tipo;
+        }
+        #endregion
+
+        #region Metodi
+        public int GetMensilità()
+        {
+            return Tipo == TipoLavoratore.Autonomo ? 12 : 13;
+        }
+        #endregion
+    }
+}
