4c829ac038ed74d001d645d4b19b8ed98c16cc8b
Roberto Spagliccia <Spacio@users.noreply.github.com>
2019-10-21 22:18:28 +0200

    Added class DbHelper.cs

---
+++ EserciziADO/Helpers/DbHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EserciziADO.Helpers
+{
+    public class DbHelper
+    {
+        public string ConnectionString { get; private set; }
+
+        public DbHelper(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+    }
+}
