5cc82148a97d3f7fc68e3e2b618d70df6c14ab38
Roberto Spagliccia <Spacio@users.noreply.github.com>
2019-10-21 23:30:46 +0200

    added logic to init db with some perone

--- EserciziADO/Program.cs
+++ EserciziADO/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+﻿using EserciziADO.Helpers;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,21 @@ namespace EserciziADO
     {
         static void Main(string[] args)
         {
+            InitDb();
+        }
+
+        static void InitDb()
+        {
+            List<Persona> listP = new List<Persona>
+            {
+                new Persona("Lord", "Voldemort", new DateTime(1926, 12, 31)),
+                new Persona("Harry", "Potter", new DateTime(1980, 7, 31))
+            };
+
+            foreach(var p in listP)
+            {
+                DbHelper.InsertPersona(p);
+            }
         }
     }
 }
